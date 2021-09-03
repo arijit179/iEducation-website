b3bf46438dabb1a065a74025efe2bfc941bfd2bd
Arijit <banerjeearijit179@gmail.com>
2021-09-03 12:46:33 +0530

    Initial Commit

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+navbar = document.querySelector('.navbar');
+burger = document.querySelector('.burger');
+rightNav = document.querySelector('.rightNav');
+navList = document.querySelector('.nav-list');
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
